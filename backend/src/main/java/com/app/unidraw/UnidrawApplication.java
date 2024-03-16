6d0b0ca772400d7879af09abcbb58239b7559bcc
Siddharth Kumar <sinhasiddharth1998@gmail.com>
2024-03-16 11:15:31 +0530

    Backend Project Setup
    Springboot Project setup with websocket depenedency

---
+++ backend/src/main/java/com/app/unidraw/UnidrawApplication.java
@@ -0,0 +1,13 @@
+package com.app.unidraw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnidrawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnidrawApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/app/unidraw/UnidrawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.unidraw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnidrawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
